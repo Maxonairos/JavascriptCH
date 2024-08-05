39333e368029d691a7a0b47f949937f0b5559a23
Fsoriano <maxonairos@gmail.com>
2024-08-05 18:25:15 -0300

    - se agregan imagenes de productos

--- js/productos.js
+++ js/productos.js
@@ -7,7 +7,7 @@ let productos = [
         precio: 1200000,
         cantidad: 2,
         descripcion: "Laptop HP con procesador Intel Core i5 y 8GB RAM",
-        img: '../img/image1.png'
+        img: '../img/image1.jpg'
     },
     {   
         id: 2,
@@ -16,7 +16,7 @@ let productos = [
         precio: 300000,
         cantidad: 6,
         descripcion: "Monitor Dell de 24 pulgadas Full HD",
-        img: '../img/image1.png'
+        img: '../img/image2.jpg'
     },
     {   
         id: 3,
@@ -25,7 +25,7 @@ let productos = [
         precio: 50000,
         cantidad: 10,
         descripcion: "Teclado inalámbrico Logitech con retroiluminación",
-        img: '../img/image1.png'
+        img: '../img/image3.jpg'
     },
     {   
         id: 4,
@@ -34,7 +34,7 @@ let productos = [
         precio: 5000,
         cantidad: 10,
         descripcion: "Mouse óptico Microsoft con 5 botones programables",
-        img: '../img/image1.png'
+        img: '../img/image4.jpg'
     },
     {
         id: 5,
@@ -43,7 +43,7 @@ let productos = [
         precio: 1800000,
         cantidad: 5,
         descripcion: "PC Gamer con Ryzen 7, RTX 3060, 16GB RAM y SSD 1TB",
-        img: '../img/image1.png'
+        img: '../img/image5.jpg'
     },
     {
         id: 6,
@@ -52,7 +52,7 @@ let productos = [
         precio: 500000,
         cantidad: 5,
         descripcion: "Monitor gaming de 27 pulgadas, 144Hz, resolución 2560x1440",
-        img: '../img/image1.png'
+        img: '../img/image6.jpg'
     },
     {
         id: 7,
@@ -61,7 +61,7 @@ let productos = [
         precio: 12000,
         cantidad: 0,
         descripcion: "Teclado mecánico RGB con switches Cherry MX Red",
-        img: '../img/image1.png'
+        img: '../img/image7.jpg'
     },
     {
         id: 8,
@@ -70,7 +70,7 @@ let productos = [
         precio: 8000,
         cantidad: 4,
         descripcion: "Mouse gaming con sensor óptico 16000 DPI",
-        img: '../img/image1.png'
+        img: '../img/image8.jpg'
     }
 ];
 
